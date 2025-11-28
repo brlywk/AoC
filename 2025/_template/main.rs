a7bf7f70a8fce7c5e754acd478ebf8897d2c5b01
Christoph Suessmeier <mail@barelyawake.de>
2025-11-28 10:57:00 +0100

    Updated 2025 Rust template setup

---
+++ 2025/_template/main.rs
@@ -0,0 +1,40 @@
+const TEST: &str = include_str!("./test.txt");
+const DATA: &str = include_str!("./data.txt");
+
+fn main() {
+    let p1 = part1(DATA);
+    println!("Part 1: {p1}");
+
+    let p2 = part2(DATA);
+    println!("Part 2: {p2}");
+}
+
+//////////////////////////////////////////////////
+// Part 1
+//////////////////////////////////////////////////
+
+fn part1(input: &str) -> String {
+    let mut result: isize = 0;
+
+    result.to_string()
+}
+
+#[test]
+fn part1_test() {
+    assert_eq!(part1(TEST), "42");
+}
+
+//////////////////////////////////////////////////
+// Part 2
+//////////////////////////////////////////////////
+
+fn part2(input: &str) -> String {
+    let mut result: isize = 0;
+
+    result.to_string()
+}
+
+#[test]
+fn part2_test() {
+    assert_eq!(part2(TEST), "42");
+}
--- 2025/day.sh
+++ 2025/day.sh
@@ -2,9 +2,15 @@
 
 if [ -z "$1" ]; then
     echo "Usage: ./day.sh <day number>"
+    exit 1
 fi
 
 day=$(printf "%02d" "$1")
 
-cargo new day$day
-echo "Created day$day"
+cargo init "day$day"
+cp _template/main.rs "day$day/src/main.rs"
+cp _template/test.txt "day$day/src/test.txt"
+cp _template/data.txt "day$day/src/data.txt"
+mv "day$day" "$day"
+
+echo "Created $day"

e0bfb65a052f98f5f11106abc1bcbaf4406b51e4
Christoph Suessmeier <christoph.suessmeier@exxeta.com>
2025-12-02 11:50:01 +0100

    2025 - day 2 - part 2

--- 2025/02/src/main.rs
+++ 2025/02/src/main.rs
@@ -31,7 +31,7 @@ fn parse_input(input: &str) -> Vec<std::ops::RangeInclusive<usize>> {
 // repeating patterns is probably just "treat number as string, split in half, check equality" :D
 //
 // Could also be a trait, but that's probably a lot of boilerplate for little practical value ;)
-fn is_repeated(num: usize) -> bool {
+fn repeated_twice(num: usize) -> bool {
     let num_str = num.to_string();
     let (a, b) = num_str.split_at(num_str.len() / 2);
 
@@ -48,7 +48,7 @@ fn part1(input: &str) -> String {
         .map(|range| {
             range
                 .clone()
-                .map(|num| if is_repeated(num) { num } else { 0 })
+                .map(|num| if repeated_twice(num) { num } else { 0 })
                 .sum::<usize>()
         })
         .sum::<usize>()
@@ -64,13 +64,43 @@ fn part1_test() {
 // Part 2
 //////////////////////////////////////////////////
 
-fn part2(input: &str) -> String {
-    let mut result: isize = 0;
+// Similar to repeated_twice(): treat num as string, and split it evenly up to its length
+// and compare all parts with each other
+fn is_invalid(num: usize) -> bool {
+    let num_str = num.to_string();
+    let l = num_str.len();
 
-    result.to_string()
+    // one of these MUST be true for the number to be invalid
+    // num splits: l/2
+    // one split is always at each digit
+    // l = 2 -> 2/2 -> 1 split  -> 11     -> 1,1 (at l/2)
+    // l = 4 -> 4/2 -> 2 splits -> 5050   -> 50,50 and 5,0,5,0 (at l/2)
+    // l = 6 -> 6/2 -> 3 splits -> 121212 -> 121, 211 / 12,12,12 / 1,2,1,2,1,2
+    // etc.
+    (1..=l / 2)
+        .filter(|pattern_length| l.is_multiple_of(*pattern_length))
+        .any(|pattern_length| {
+            let pattern = &num_str[..pattern_length];
+            num_str == pattern.repeat(l / pattern_length)
+        })
+    // at this point I would like to thank clippy's pedantic settings for letting me learn
+    // MANY new stdlib functions (e.g. is_multiple_of) ;)
+}
+
+fn part2(input: &str) -> String {
+    parse_input(input.trim())
+        .iter()
+        .map(|range| {
+            range
+                .clone()
+                .map(|num| if is_invalid(num) { num } else { 0 })
+                .sum::<usize>()
+        })
+        .sum::<usize>()
+        .to_string()
 }
 
 #[test]
 fn part2_test() {
-    assert_eq!(part2(TEST), "42");
+    assert_eq!(part2(TEST), "4174379265");
 }

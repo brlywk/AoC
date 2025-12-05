f9ce5ee47828b91dec6e33190b0d6b97c2a659c0
Christoph Suessmeier <christoph.suessmeier@exxeta.com>
2025-12-05 09:53:40 +0100

    2025 - day 5 - part 2

--- 2025/05/src/main.rs
+++ 2025/05/src/main.rs
@@ -1,6 +1,5 @@
-use std::ops::RangeInclusive;
-
 use anyhow::anyhow;
+use std::ops::RangeInclusive;
 
 #[allow(dead_code)]
 const TEST: &str = include_str!("./test.txt");
@@ -55,22 +54,65 @@ impl Inventory {
         })
     }
 
-    fn count_fresh(&self) -> usize {
+    fn fresh_ingredients(&self) -> usize {
         self.food_ids
             .iter()
             .filter(|id| self.fresh_ranges.iter().any(|range| range.contains(*id)))
             .count()
     }
+
+    fn count_fresh_ids(&self) -> usize {
+        // This doesn't work b/c it expands all ranges; and with the large numbers AoC uses
+        // this takes A LOT of memory :(
+        // self.fresh_ranges
+        //     .iter()
+        //     .flat_map(Clone::clone)
+        //     .collect::<HashSet<usize>>()
+        // .len()
+
+        // So: merge overlapping ranges and count elements in each
+
+        // take only the first and last number as a tuple and sort by start number
+        // so when we iterate over these ranges, we can check if subsequent ranges
+        // fall (i.e. start) within the "current range"
+        let mut sorted_ranges: Vec<_> = self
+            .fresh_ranges
+            .iter()
+            .map(|range| (*range.start(), *range.end()))
+            .collect();
+        sorted_ranges.sort_by_key(|r| r.0);
+
+        let mut merged_ranges = vec![];
+        let (mut current_start, mut current_end) = sorted_ranges[0];
+
+        // iterate over all ranges and check if these ranges can be merged together:
+        // take the lowest start and highest end of overlapping/adjacent ranges
+        for &(start, end) in &sorted_ranges[1..] {
+            if start <= current_end + 1 {
+                current_end = current_end.max(end);
+            } else {
+                merged_ranges.push(current_start..=current_end);
+                current_start = start;
+                current_end = end;
+            }
+        }
+        merged_ranges.push(current_start..=current_end);
+
+        // now we can just count the numbers without counting the numbers :P
+        merged_ranges
+            .iter()
+            .map(|range| range.end() - range.start() + 1)
+            .sum()
+    }
 }
 
 fn part1(input: &str) -> anyhow::Result<String> {
-    Ok(Inventory::new(input)?.count_fresh().to_string())
+    Ok(Inventory::new(input)?.fresh_ingredients().to_string())
 }
 
 #[test]
 fn part1_test() -> anyhow::Result<()> {
     assert_eq!(part1(TEST)?, "3");
-
     Ok(())
 }
 
@@ -79,13 +121,11 @@ fn part1_test() -> anyhow::Result<()> {
 //////////////////////////////////////////////////
 
 fn part2(input: &str) -> anyhow::Result<String> {
-    let mut result: isize = 0;
-
-    Ok(result.to_string())
+    Ok(Inventory::new(input)?.count_fresh_ids().to_string())
 }
 
 #[test]
 fn part2_test() -> anyhow::Result<()> {
-    assert_eq!(part2(TEST)?, "42");
+    assert_eq!(part2(TEST)?, "14");
     Ok(())
 }

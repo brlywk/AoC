713faeb2382c817d8023df2458ae29157770dfed
Christoph Suessmeier <christoph.suessmeier@exxeta.com>
2025-12-12 08:07:58 +0100

    2025 - day 11 - part 2

--- 2025/11/src/main.rs
+++ 2025/11/src/main.rs
@@ -1,7 +1,8 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 #[allow(dead_code)]
 const TEST: &str = include_str!("./test.txt");
+const TEST2: &str = include_str!("./test2.txt");
 const DATA: &str = include_str!("./data.txt");
 
 fn main() {
@@ -72,11 +73,11 @@ fn find_all_paths(
     all_paths
 }
 
-const START: &str = "you";
-const END: &str = "out";
+const START_P1: &str = "you";
+const END_P1: &str = "out";
 
 fn part1(input: &'static str) -> String {
-    find_all_paths(&parse_input(input), START, END)
+    find_all_paths(&parse_input(input), START_P1, END_P1)
         .iter()
         .len()
         .to_string()
@@ -91,13 +92,56 @@ fn part1_test() {
 // Part 2
 //////////////////////////////////////////////////
 
-fn part2(input: &str) -> String {
-    let mut result: isize = 0;
+const START_P2: &str = "svr";
+const END_P2: &str = "out";
+const FFT: &str = "fft";
+const DAC: &str = "dac";
 
-    result.to_string()
+// try essentially the solution from part 1, but don't store, just count paths
+fn count_paths(
+    devices: &[Device],
+    start: &str,
+    end: &str,
+    must_visit_1: &str,
+    must_visit_2: &str,
+) -> usize {
+    // convert to lookup map for devices by label
+    let device_map: HashMap<&str, &Device> = devices.iter().map(|d| (d.label, d)).collect();
+
+    // to save some time, store a HashSet that just checks if a path visits both "must_visits"
+    let mut count = 0;
+    let mut stack = vec![(start, HashSet::from([start]), false, false)];
+
+    while let Some((current, visited, has_1, has_2)) = stack.pop() {
+        let new_has_1 = has_1 || current == must_visit_1;
+        let new_has_2 = has_2 || current == must_visit_2;
+
+        if current == end {
+            if new_has_1 && new_has_2 {
+                count += 1;
+            }
+            continue;
+        }
+
+        if let Some(device) = device_map.get(current) {
+            for &output in &device.outputs {
+                if !visited.contains(output) {
+                    let mut new_visited = visited.clone();
+                    new_visited.insert(output);
+                    stack.push((output, new_visited, new_has_1, new_has_2));
+                }
+            }
+        }
+    }
+
+    count
+}
+
+fn part2(input: &'static str) -> String {
+    count_paths(&parse_input(input), START_P2, END_P2, FFT, DAC).to_string()
 }
 
 #[test]
 fn part2_test() {
-    assert_eq!(part2(TEST), "42");
+    assert_eq!(part2(TEST2), "2");
 }
